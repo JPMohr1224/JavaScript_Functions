444bd8680c66c7dd18f21ea4709225dc8f0ffb92
JP Mohr <James.Mohr2.0@hotmail.com>
2024-06-07 14:43:50 -0400

    exercise 1 & 2

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 console.log("Hello World!\n==========\n");
 
+
+function printOdds(count) {
+    for(let i = 1; i <= count; i++) {
+        if (i % 2 != 0) {
+            console.log(i)
+        }
+    }
+}
+
+printOdds(100)
+
+
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log("EXERCISE 1 ABOVE:\n==========\n");
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+function checkAge(name, age){
+    
+    if ( age >= 16) {
+       let aboveSixteen = (`Congrats ${name}, you can drive!`)
+       console.log(aboveSixteen)
+    } else {
+        let belowSixteen = (`Sorry ${name}, but you need to wait until you're 16.`)
+        console.log(belowSixteen)
+    }
+}
+
+
+checkAge("JP", )
